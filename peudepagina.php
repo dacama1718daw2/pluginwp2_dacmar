994c69ecce5484ec6535d302d6305b3c398de755
root <root@debian9v1.fjeclot.net>
2018-04-20 20:05:32 +0200

    commit 1

---
+++ peudepagina.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: https://github.com/dacama1718daw2/pluginwp2_dacmar
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: David Castillo
+Author URI: https://github.com/dacama1718daw2
+License: GNU GPLv3 
+*/
+ 
+?>
